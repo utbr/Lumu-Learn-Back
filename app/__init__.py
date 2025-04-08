36754154872bf77d6458c670a4d8e3bd55020869
Leonardo Felipe Hopata Machado <140458056+utbr@users.noreply.github.com>
2025-04-08 08:30:58 -0300

    commit 4

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from .routes import configure_routes
 
 def create_app():
     app = Flask(__name__)
+
+    # LIBERA a origem do frontend (http://localhost:5173)
+    CORS(app, resources={r"/*": {"origins": "http://localhost:5173"}})
+
     configure_routes(app)
     return app
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
 
+# Usuários simulados
 Usuarios = [
     {
         'id': 1,
@@ -10,5 +11,18 @@ Usuarios = [
 
 def configure_routes(app):
     @app.route('/login', methods=['GET'])
-    def obter_usuario():
+    def obter_usuarios():
         return jsonify(Usuarios)
+
+    @app.route('/login', methods=['POST'])
+    def verificar_login():
+        dados = request.get_json()
+
+        usuario_input = dados.get('Usuario')
+        senha_input = dados.get('Senha')
+
+        for usuario in Usuarios:
+            if usuario['Usuario'] == usuario_input and usuario['Senha'] == senha_input:
+                return jsonify({'mensagem': 'Login feito com sucesso', 'status': 'sucesso'}), 200
+
+        return jsonify({'mensagem': 'Usuário ou senha incorretos', 'status': 'erro'}), 401
